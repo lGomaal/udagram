a1de83fa22501fcdb921fc8b6c926ebca30663dd
lGomaal <Gomaa.fcis@hotmail.com>
2022-11-22 03:29:43 +0200

    adding updates to the main .sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
+export PPOSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=udagram.cfzkcbor6w5p.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::front-bucket-28342289234
+export AWS_BUCKET=arn:aws:s3:::media-bucket123213123
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://localhost:4200
